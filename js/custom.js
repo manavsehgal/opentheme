ad7ac590a99a8f288de5f6b92c09955552b2a91e
manavsehgal <sehgal.manav@gmail.com>
2014-11-24 13:28:56 +0000

    Added statistics, Updated themes folder, removed flip JS

--- js/custom.js
+++ js/custom.js
@@ -6,22 +6,6 @@ $(document)
 *******************************/
 
     var
-      changeSides = function() {
-        $('.ui.shape')
-          .eq(0)
-            .shape('flip over')
-            .end()
-          .eq(1)
-            .shape('flip over')
-            .end()
-          .eq(2)
-            .shape('flip back')
-            .end()
-          .eq(3)
-            .shape('flip back')
-            .end()
-        ;
-      },
       validationRules = {
         firstName: {
           identifier  : 'email',
@@ -55,7 +39,6 @@ $(document)
       .transition('scale in', 1000)
     ;
 
-    setInterval(changeSides, 3000);
 
 /*******************************
           Feed

cbad76abd6e85c2dcc4c861a4a89805c8fa3459f
manavsehgal <sehgal.manav@gmail.com>
2014-12-16 08:32:53 +0000

    MailChimp integrated

--- js/landing.js
+++ js/landing.js
@@ -1,28 +1,4 @@
 $(document)
   .ready(function() {
 
-    var
-      validationRules = {
-        firstName: {
-          identifier  : 'email',
-          rules: [
-            {
-              type   : 'empty',
-              prompt : 'Please enter an e-mail'
-            },
-            {
-              type   : 'email',
-              prompt : 'Please enter a valid e-mail'
-            }
-          ]
-        }
-      }
-    ;
-
-    $('.ui.form')
-      .form(validationRules, {
-        on: 'blur'
-      });
-
-
 });

455ba3a493bdfdf8e5cb027048a6b0bc4b3bdb6a
manavsehgal <sehgal.manav@gmail.com>
2014-12-23 17:52:50 +0000

    Carrot Share Button for Posts

--- js/post.js
+++ js/post.js
@@ -3,5 +3,18 @@ $(document).ready(function() {
   $(".time").text(function (index, value) {
     return Math.round(parseFloat(value));
   });
+  
+  config = {
+      ui: {
+        flyout: "middle right"
+      },
+      networks: {
+          facebook: {
+              app_id: "390272554479803"
+          }
+      }
+  }
+  
+  var share = new Share('.share-button', config);
 
 });

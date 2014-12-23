e71778c70294ab8c76d801901a4c428e6efcee90
manavsehgal <sehgal.manav@gmail.com>
2014-12-23 04:07:40 +0000

    Flexslider added. Apps landing uses slider.

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: true,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: true,
+    sync: "#carousel"
+  });
+});

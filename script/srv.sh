ade45732f6912bc7bd829225e25f79be9335e373
manavsehgal <sehgal.manav@gmail.com>
2014-12-25 12:14:59 +0000

    Home stats ribbon divided

--- script/srv.sh
+++ script/srv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build
+# bundle exec jekyll build
 # bundle exec htmlproof ./_site
 bundle exec jekyll serve --port 8080 --host 0.0.0.0 --safe --baseurl ''
